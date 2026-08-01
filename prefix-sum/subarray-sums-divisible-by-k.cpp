28932a2788cc8e2d44d567e477997f51172e3f7a
shivam maurya <aniheshmaurya456@gmail.com>
2026-08-01 11:04:17 +0530

    LC-subarray-sums-divisible-by-k [Runtime: N/A; Memory: N/A]

--- prefix-sum/subarray-sums-divisible-by-k.cpp
+++ prefix-sum/subarray-sums-divisible-by-k.cpp
@@ -2,16 +2,25 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n=nums.size();
-        int ans = 0;
+        int sum = 0;
+        unordered_map<int , int >mp;
+        mp[0]=1;
+        int count = 0;
         for(int i=0 ; i<n ; i++){
-            int sum=0;
-            for(int j=i ; j<n ; j++){
-                sum += nums[j];
-                if(sum%k==0){
-                    ans++;
-                }
+            sum += nums[i];
+
+            int rem = sum%k;
+            if(rem<0){
+                rem += k;
             }
+
+            if(mp.find(rem)!= mp.end()){
+                count += mp[rem];
+                
+            }
+             mp[rem]++;
         }
-        return ans;
+            return count;
+            
     }
 };

45537bf7ef2ee3aec612b1e60babd2b47b174d96
shivam maurya <aniheshmaurya456@gmail.com>
2026-08-01 13:22:25 +0530

    LC-subarray-sum-equals-k [Runtime: 43 ms; Memory: 45.4 MB]

--- array/subarray-sum-equals-k.cpp
+++ array/subarray-sum-equals-k.cpp
@@ -3,16 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
        int n=nums.size();
        int countsub = 0;
+       int sum = 0;
+       unordered_map<int , int>mp;
+       mp[0]=1;
        for(int i=0 ; i<n ; i++){
-          int sum=0;
-          for(int j=i ; j<n ; j++){
-            sum += nums[j];
-
-            if(sum==k){
-                countsub++;
-            }
-          }
-       } 
-       return countsub;
+        sum += nums[i];
+        int pre = sum-k;
+        if(mp.find(pre)!=mp.end()){
+            countsub += mp[pre];
+        }
+        mp[sum]++;
+               }
+               return countsub;
     }
 };

77150fe850cb3d397080fc5df4a185cf3ebbe9b0
shivam maurya <aniheshmaurya456@gmail.com>
2026-08-08 09:45:32 +0530

    LC-matrix-diagonal-sum [Runtime: 0 ms; Memory: 15 MB]

--- array/matrix-diagonal-sum.cpp
+++ array/matrix-diagonal-sum.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
         int n = mat.size();
+       
         int sum = 0;
-
-        for (int i=0 ; i<n ; i++){
-            sum += mat[i][i];
-            sum += mat[i][n-1-i];
+        for(int i=0 ; i<n ; i++){
+           
+            for(int j=0; j<n ; j++){
+                if(i==j){
+                    sum += mat[i][j];
+                }else if(j==n-1-i){
+                    sum += mat[i][j];
+                }
+            }
+        
         }
-
-        if(n%2==1){
-            sum -= mat[n/2][n/2];
-        }
+     
         return sum;
     }
 };
